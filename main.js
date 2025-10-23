2227aebb9f5e6702dc5af5a7d18f769e35ffadff
Arellano136 <132949146+Arellano136@users.noreply.github.com>
2025-10-22 22:06:12 -0600

    estilos css

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('./sw.js') // ✅ ruta relativa
+      .register('./sw.js') 
       .then(reg => console.log('Registro SW exitoso: ', reg))
       .catch(err => console.error('Error de registro SW: ', err));
   });
